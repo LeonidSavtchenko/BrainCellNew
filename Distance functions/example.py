ba36e6971ba7320268a148ac19916353433ae473
Savtchenko <savtchenkolp@gmail.com>
2023-06-16 11:10:54 +0100

    update

--- Distance functions/example.py
+++
@@ -1,7 +0,0 @@
-
-from math import *
-
-def myDistFunc(x):
-    y = x/10 + 50*sin(2*pi*x/200) * tanh(x/100)
-    return y
-    
--- build_mechs.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd "Mechanisms/Astrocyte"
-call build_astrocyte_mechs.bat 1
-if errorlevel 1 goto label
-cd ../Neuron
-call build_neuron_mechs.bat 1
-:label
-popd
-pause
